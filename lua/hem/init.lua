9becbc40cb5f750aed157ce4f574ff88bc3362c8
Tom Bell <tomb@tombell.dev>
2026-02-08 20:10:55 +0000

    Refactor to use vim.g for options

--- lua/hem/init.lua
+++ lua/hem/init.lua
@@ -1,19 +1,45 @@
-local Components = require "hem.components"
-local Config = require "hem.config"
-
 local M = {}
 
----@param opts? hem.Config
-function M.setup(opts)
-  require("hem.config").setup(opts)
+---@class hem.Opts
+---
+--- The components for the status column.
+---@field components? string[]
+---
+--- The configuration for the fold component.
+---@field fold table<"open"|"close", string>
+
+---@type hem.Opts
+local defaults = {
+  components = { "%l", "%s", "{fold}", " " },
+  fold = {
+    open = "",
+    close = "",
+  },
+}
+
+---@type hem.Opts|nil
+vim.g.hem_opts = vim.g.hem_opts
+
+M.opts = vim.tbl_deep_extend("force", vim.deepcopy(defaults), vim.g.hem_opts or {})
+
+function M.fold()
+  return vim.api.nvim_win_call(vim.g.statusline_winid, function()
+    if vim.fn.foldclosed(vim.v.lnum) >= 0 then
+      return M.opts.fold.close
+    elseif tostring(vim.treesitter.foldexpr(vim.v.lnum)):sub(1, 1) == ">" then
+      return M.opts.fold.open
+    else
+      return " "
+    end
+  end)
 end
 
 ---@return string
 function M.get()
   return table.concat(
     vim.tbl_map(function(component)
-      return component == "{fold}" and Components.fold() or component
-    end, Config.components),
+      return component == "{fold}" and M.fold() or component
+    end, M.opts.components),
     " "
   )
 end
